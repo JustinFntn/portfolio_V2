6235e98c06bd60425bdd003a8f96be386de91382
jfontaine <j.fontaine@atmo-hdf.fr>
2025-06-20 10:43:33 +0200

    deployment

--- lib/types.ts
+++
@@ -1,6 +0,0 @@
-import { links } from './data';
-import { StaticImageData } from 'next/image';
-
-export type SectionName = (typeof links)[number]["name"];
-
-export type SkillIcon = string | StaticImageData;
